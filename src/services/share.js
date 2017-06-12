9d48460747ec06d054fb20bdf6e96ec635aa6e82
Pasi Lampinen <plam@fl027.local>
2017-06-12 10:25:02 +0300

    Remove wappu.futurice.com from sharing urls

--- src/services/share.js
+++ src/services/share.js
@@ -1,6 +1,10 @@
 export const getShareUrl = (url) => {
+	return url;
+
+	/*
 	const urlParts = url.split('/');
 	const imageId = urlParts[urlParts.length - 1];
 
 	return 'https://wappu.futurice.com/i/' + imageId;
+	*/
 }
