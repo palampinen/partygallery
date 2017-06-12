07635f2bb1cb6a3156b2e3c7cb12bfb4cc9ab882
Pasi Lampinen <plam@fl027.local>
2017-06-12 10:25:29 +0300

    Disable city fetching

--- src/concepts/app.js
+++ src/concepts/app.js
@@ -4,7 +4,10 @@ import { fetchFeed } from './feed';
 // # Action Creators
 export const startApp = () => (dispatch, getState) => {
   console.log('Start App');
+	return dispatch(fetchFeed());
+	/*
   return Promise.resolve(
     dispatch(fetchCities())
   ).then(() => dispatch(fetchFeed()));
+  */
 }
